e5721fb0813b794337036e50adc54baaa8ac9145
Ash-2k3 <ash.me.cse@gmail.com>
2022-11-27 15:50:14 +0530

    Bottom Navigation Implemented and corresponing empty fragments have been creaated

--- app/src/main/java/com/example/ambuxproject/views/HomeFragment.kt
+++ app/src/main/java/com/example/ambuxproject/views/HomeFragment.kt
@@ -1,28 +1,56 @@
 package com.example.ambuxproject.views
 
+import android.os.Binder
 import android.os.Bundle
 import android.view.*
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.replace
 import androidx.navigation.fragment.findNavController
 import com.example.ambuxproject.R
+import com.example.ambuxproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-   private var tvTest : TextView? = null
+
+  private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+         binding = FragmentHomeBinding.inflate(layoutInflater)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // for the first time when we will be opening the app we want to show the portion of ambulance/Map
+         replaceFragment(AppMainServicesHomeFragment())
+
+        //trigger and navigate accordingly according to the item selected in bottom navigation menu
+        binding.bottomNavigationMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.fragmet_settings -> replaceFragment(AppSettingsFragment())
+                R.id.fragment_map_main -> replaceFragment(AppMainServicesHomeFragment())
+                R.id.fragment_person_details -> replaceFragment(UserDetailsFragment())
+                else ->{
+
+                }
+            }
+         true
+        }
+
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        tvTest = view.findViewById(R.id.tv_test)
-        super.onViewCreated(view, savedInstanceState)
+
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = childFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_container_of_home_fragment,fragment)
+        fragmentTransaction.commit()
     }
+
 
 
 
